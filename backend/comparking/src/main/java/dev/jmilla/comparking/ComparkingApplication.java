07f20856ed09ce50e4c1083f248fffe8a84ba2cb
Jesús Milla <jmilcab353@g.educaand.es>
2025-04-22 20:24:40 +0200

    backend: spring boot structure added

---
+++ backend/comparking/src/main/java/dev/jmilla/comparking/ComparkingApplication.java
@@ -0,0 +1,13 @@
+package dev.jmilla.comparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComparkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComparkingApplication.class, args);
+	}
+
+}
---
+++ backend/comparking/src/test/java/dev/jmilla/comparking/ComparkingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jmilla.comparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComparkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
