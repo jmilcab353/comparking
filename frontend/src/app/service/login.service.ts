f1425281287b9b8a8c6c99ab4dc9d5baacd6eb67
Jesús Milla <jmilcab353@g.educaand.es>
2025-06-09 19:48:34 +0200

    frontend: login service

--- frontend/src/app/service/login.service.ts
+++ frontend/src/app/service/login.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map, Observable, throwError } from 'rxjs';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +8,9 @@ import { map, Observable, throwError } from 'rxjs';
 export class LoginService {
 
   token: string;
-  perfil: string; // admin, experto
+  perfil: string;
   logged: boolean;
-  user: any; // objeto con los datos del usuario
-
+  user: any;
 
   constructor(private http: HttpClient) {
     this.token = "";
@@ -21,58 +20,44 @@ export class LoginService {
   }
 
   private almacenar() {
-    var objeto: any;
-    objeto = {
+    const objeto = {
       token: this.token,
       perfil: this.perfil,
       logged: this.logged,
       user: this.user
-    }
+    };
     sessionStorage.setItem("LOGIN", JSON.stringify(objeto));
   }
 
   recuperar() {
-
-    var cadena: any;
-
-    cadena = sessionStorage.getItem("LOGIN") || "";
+    const cadena = sessionStorage.getItem("LOGIN") || "";
 
     if (cadena != "") {
-
-      var objeto = JSON.parse(cadena);
-
+      const objeto = JSON.parse(cadena);
       this.token = objeto.token;
       this.perfil = objeto.perfil;
       this.logged = objeto.logged;
       this.user = objeto.user;
-
     } else {
-
       this.token = "";
       this.perfil = "";
       this.logged = false;
       this.user = {};
-
     }
-
   }
 
   login(user: string, pass: string) {
-    let objeto: any = this;
-
     return this.http.post("http://localhost:9000/auth/login", {
-      // return this.http.post("http://localhost/servidor_skills/login.php", {
       username: user,
       password: pass
     }).pipe(
       map((data: any) => {
         if (data != null && data.token != "") {
-          // console.log(data);
-          objeto.user = { nombre: data.user };
-          objeto.perfil = data.role;
-          objeto.logged = true;
-          objeto.token = data.token;
-          objeto.almacenar();
+          this.user = { nombre: data.username };
+          this.perfil = data.role;
+          this.logged = true;
+          this.token = data.token;
+          this.almacenar();
           return { funciona: true, perfil: data.role };
         } else {
           return { funciona: false };
@@ -85,63 +70,22 @@ export class LoginService {
     sessionStorage.removeItem("LOGIN");
   }
 
-  // logout() {
-
-  //   let objeto: any = this;
-  //   let contenido: string | null = sessionStorage.getItem("LOGIN");
-
-  //   this.http.get("http://localhost/servidor_skills/login.php?desloguear=" + JSON.parse(contenido || '').token)
-  //     .subscribe(function (data: any) {
-  //       objeto.machacar();
-  //     })
-  // }
-
   logout() {
-    let objeto: any = this;
-
-    // Eliminar los datos de sesión (el token y otros datos relacionados)
-    sessionStorage.removeItem("LOGIN");
-
-    // Actualizar el estado del cliente para reflejar que ya no está logueado
-    objeto.machacar();
+    this.machacar();
   }
 
   isLogged(): boolean {
-
-    let respuesta: boolean = false;
-    let contenido: string | null = sessionStorage.getItem("LOGIN");
-
-    if (contenido) {
-      respuesta = JSON.parse(contenido || '').logged;
-    }
-
-    return respuesta;
+    const contenido = sessionStorage.getItem("LOGIN");
+    return contenido ? JSON.parse(contenido).logged : false;
   }
 
   getNombre(): string {
-
-    let respuesta: string = '';
-    let contenido: string | null = sessionStorage.getItem("LOGIN");
-
-    if (contenido) {
-      respuesta = JSON.parse(contenido || '').user.nombre;
-    }
-
-    return respuesta;
-
+    const contenido = sessionStorage.getItem("LOGIN");
+    return contenido ? JSON.parse(contenido).user.nombre : '';
   }
 
   getRole(): string {
-
-    let contenido: string | null = sessionStorage.getItem("LOGIN");
-    let respuesta: string = "";
-
-    if (contenido) {
-      respuesta = JSON.parse(contenido || '').perfil;
-    }
-
-    return respuesta;
-
+    const contenido = sessionStorage.getItem("LOGIN");
+    return contenido ? JSON.parse(contenido).perfil : '';
   }
-
 }
